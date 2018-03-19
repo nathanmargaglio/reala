e8b8eb26cc8df78641d8e0cf9276dd034a67ba82
Nathan <nathanmargaglio@gmail.com>
2018-03-19 16:25:24 -0400

    fixed commented out db setting

--- server/bfds/settings.py
+++ server/bfds/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
 
 if not dev_mode:
     import dj_database_url
-    #db_from_env = dj_database_url.config(conn_max_age=500)
-    #DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
